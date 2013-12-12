a52ef69c1cabfc9b7aeb435f4e2f124d83faffcd
Jeromy <kerosuppi@gmail.com>
2013-12-11 19:41:17 -0800

    Formatting fix on Client.rb => tabs to spaces

--- lib/mumble-ruby/client.rb
+++ lib/mumble-ruby/client.rb
@@ -135,7 +135,7 @@ module Mumble
         @channels.delete(message.channel_id)
       end
       on_user_state do |message|
-	@users[message.session] = message
+        @users[message.session] = message
       end
       on_user_remove do |message|
         @users.delete(message.session)
