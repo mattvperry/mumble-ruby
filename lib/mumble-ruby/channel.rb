9636e64f4ac022875e65258fc46a92d5d7243e4d
Jack Chen (chendo) <github@chen.do>
2014-07-12 16:15:48 +1000

    Add more feature parity on the models

--- lib/mumble-ruby/channel.rb
+++ lib/mumble-ruby/channel.rb
@@ -1,6 +1,8 @@
 module Mumble
   class Channel < Model
-    attribute :channel_id
+    attribute :channel_id do
+      self.data.fetch(:channel_id, 0)
+    end
     attribute :name
     attribute :parent_id do
       self.data['parent']
@@ -11,9 +13,27 @@ module Mumble
     end
 
     def children
-      client.channels.select do |channel|
+      client.channels.values.select do |channel|
         channel.parent_id == channel_id
       end
+    end
+
+    def users
+      client.users.values.select do |user|
+        user.channel_id == channel_id
+      end
+    end
+
+    def join
+      client.join_channel(self)
+    end
+
+    def send_text(string)
+      client.text_channel(self, string)
+    end
+
+    def send_image(file)
+      client.text_channel_img(self, file)
     end
   end
 end
--- lib/mumble-ruby/user.rb
+++ lib/mumble-ruby/user.rb
@@ -7,5 +7,13 @@ module Mumble
     def channel
       client.channels[channel_id]
     end
+
+    def send_text(string)
+      client.text_user(self, string)
+    end
+
+    def send_image(file)
+      client.text_user_img(self, file)
+    end
   end
 end
