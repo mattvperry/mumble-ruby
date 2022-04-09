f0529e251efc7f989794f6dcf2ca1991a4d7b773
dafoxia <dafoxia@users.noreply.github.com>
2022-04-09 13:25:44 +0200

    Update connection.rb
    Allow other (newer) encryption variants. Fix Bug where the mumble server reject TLSv1.

--- lib/mumble-ruby/connection.rb
+++ lib/mumble-ruby/connection.rb
@@ -13,7 +13,7 @@ module Mumble
     end
 
     def connect
-      context = OpenSSL::SSL::SSLContext.new(:TLSv1)
+      context = OpenSSL::SSL::SSLContext.new
       context.verify_mode = OpenSSL::SSL::VERIFY_NONE
       [:key, :cert].each { |s| context.send("#{s}=", @cert_manager.send(s)) }
       begin
